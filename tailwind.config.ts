4ced762e8d58765981f4af217205e9f971379433
Nalla_Rupak <rupaknalla1034@gmail.com>
2024-09-03 20:02:44 +0530

    hero-section done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'regal-blue': '#1400FF',
+      },
+      borderRadius: {
+        'custom': '25% 63% 75% 19% / 25% 45% 52% 75%',  // Add your custom radius here
+      },
     },
   },
   plugins: [],
